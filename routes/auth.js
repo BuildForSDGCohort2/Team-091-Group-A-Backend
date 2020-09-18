49c41acab55a0add66dd336ea0c12bf992a247d1
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-09-18 16:19:57 +0100

    fix login error

--- routes/auth.js
+++ routes/auth.js
@@ -16,18 +16,30 @@ function validate(req) {
 router.post("/", async (req, res) => {
   const error  = validate(req.body);
   if (error.error) {
-    return res.status(400).send(error.error.details[0].message);
+    return res.status(400).json({
+      status: "error",
+      message: error.error.details[0].message
+    });
   }
   let user = await User.findOne({ email: req.body.email });
   if (!user) {
-    return res.status(400).send("Invalid email or password....");
+    return res.status(400).json({
+      status: "error",
+      message:"Invalid email or password...."
+    });
   }
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) {
-    return res.status(400).send("Invalid email or password.....");
+    return res.status(400).json({
+      status: "error",
+      message:"Invalid email or password...."
+    });
   }
   const token = user.generateAuthKey();
-  res.header("x-auth-token", token).send({ token });
+  res.header("x-auth-token", token).json({
+    status: "success",
+    token 
+  });
 });
 
 module.exports = router;
